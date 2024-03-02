2a2f107dc47e3aff3f3cc3d602010dad54cc30c0
TungVLe <tunle0801@gmail.com>
2024-03-02 15:52:05 -0500

    flight sim banner

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,7 +58,7 @@ class Banner extends HTMLElement {
           margin: 0;
           padding: 0;
           position:absolute; left: 0; top: 0; right: 0; bottom: 0;
-          width: 98vw; height: 100vh; 
+          width: 98vw; height: 75vh; 
           background: url(${src});
           background-attachment: fixed;
           background-position: center;
