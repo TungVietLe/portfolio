ea44c89217c113cdc0b34612089656cd956f82ce
TungVLe <tunle0801@gmail.com>
2024-06-20 14:38:06 +0700

    fix: back btn doesn't work on first page
    `history.back()` doesnt work when it's being called as the first tab opened (e.g target = _blank). Added the default location to go back incase there are no previous page

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,7 +34,7 @@ customElements.define('google-analytics', GoogleAnalytics)
 class BackToMain extends HTMLElement {
 	connectedCallback() {
 		this.innerHTML = `
-        <a href="javascript:history.back()" style="display: inline-block; border-radius: 5px; font-size: large;  border: 2px solid hsl(0, 0%, 50%); color: hsl(0, 0%, 75%); padding: 10px 30px; margin-bottom: 6%; ">🡤 Back</a>
+        <a href="javascript:history.length > 1 ? history.back() : location.href='/';" style="display: inline-block; border-radius: 5px; font-size: large;  border: 2px solid hsl(0, 0%, 50%); color: hsl(0, 0%, 75%); padding: 10px 30px; margin-bottom: 6%; ">🡤 Back</a>
     `;
 	}
 }
